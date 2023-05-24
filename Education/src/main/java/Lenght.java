a7640e931cd985af5bd27bf48b778d92547cc144
Ihtiozavra <134500148+Ihtiozavra@users.noreply.github.com>
2023-05-24 14:08:08 +0300

    Создан проект Education

---
+++ Education/src/main/java/Lenght.java
@@ -0,0 +1,9 @@
+public class Lenght {
+
+    public static void main (String[] Args) {
+        Point p1 = new Point(2.0, 8.0);
+        Point p2 = new Point(6.0, 5.0);
+
+        System.out.println (Point.distance(p1, p2));
+    }
+}
---
+++ Education/src/main/java/Square.java
@@ -0,0 +1,11 @@
+public class Square {
+
+    public double l;
+
+    public Square(double l) { // Конструктор
+        this.l=l; // Первый - атрибут, второй - значение
+    }
+    public double area() {
+        return this.l * this.l;
+    }
+}
